e908c79d3f082fda9a3131e4fe2310ecb721215d
Vlad Gorbunov <vadikgo@gmail.com>
2017-05-10 17:30:00 +0300

    don't show skipped tasks

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # ansible-realtime-out
 
-Demo of the ansible realtime output from shell command
+Demo of the ansible realtime output from shell command.
+
+Example:
+
+`ansible-playbook demo.yml`
--- callback_plugins/simpleout.py
+++ callback_plugins/simpleout.py
@@ -16,6 +16,12 @@ class CallbackModule(CallbackModule_default):
     CALLBACK_TYPE = 'stdout'
     CALLBACK_NAME = 'simpleout'
 
+    def __init__(self):
+        self.super_ref = super(CallbackModule, self)
+        self.super_ref.__init__()
+        self.last_task = None
+        self.shown_title = False
+
     def _dump_results(self, result):
         '''Return the text to output for a result.'''
 
@@ -41,5 +47,44 @@ class CallbackModule(CallbackModule_default):
         if result._result['stdout']:
             self._display.display(result._result['stdout'], color=C.COLOR_OK)
 
+    def v2_playbook_on_handler_task_start(self, task):
+        self.super_ref.v2_playbook_on_handler_task_start(task)
+        self.shown_title = True
+
+    def v2_playbook_on_task_start(self, task, is_conditional):
+        self.last_task = task
+        self.shown_title = False
+
+    def display_task_banner(self):
+        if not self.shown_title:
+            self.super_ref.v2_playbook_on_task_start(self.last_task, None)
+            self.shown_title = True
+
+    def v2_runner_on_failed(self, result, ignore_errors=False):
+        self.display_task_banner()
+        self.super_ref.v2_runner_on_failed(result, ignore_errors)
+
+    def v2_runner_on_ok(self, result):
+        self.display_task_banner()
+        self.super_ref.v2_runner_on_ok(result)
+
+    def v2_runner_on_unreachable(self, result):
+        self.display_task_banner()
+        self.super_ref.v2_runner_on_unreachable(result)
+
+    def v2_runner_item_on_ok(self, result):
+        self.display_task_banner()
+        self.super_ref.v2_runner_item_on_ok(result)
+
+    def v2_runner_item_on_failed(self, result):
+        self.display_task_banner()
+        self.super_ref.v2_runner_item_on_failed(result)
+
+    def v2_runner_on_skipped(self, result):
+        pass
+
     def v2_runner_item_on_skipped(self, result):
+        pass
+
+    def v2_playbook_on_include(self, included_file):
         pass
